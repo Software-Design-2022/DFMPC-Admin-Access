001a6ab35397fe4f6d8c5df8aa98e46a34e86fb9
Nokuthaba <710178@students.wits.ac.za>
2022-08-30 15:55:27 +0200

    Tests
    Testing for Logic Functions as well as testing to make sure data is added to firebase

---
+++ protocols.html.test.js
@@ -0,0 +1,28 @@
+import { fireEvent, getByText } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+import { assert } from 'console'
+
+const html = fs.readFileSync(path.resolve(__dirname, './protocols.html'), 'utf8');
+
+let dom
+let container 
+
+
+describe('protocols.html', () => {
+
+    it('renders a table element', () => {
+        dom = new JSDOM(html)
+        container = dom.window.document.body
+        expect(container.querySelector('table')).toBeInTheDocument()
+    })
+
+    it('renders a button element', () => {
+        dom = new JSDOM(html)
+        container = dom.window.document.body
+        expect(container.querySelector('button')).toBeInTheDocument()
+    })
+    
+})
--- student_schedule.html.test.js
+++ student_schedule.html.test.js
@@ -6,6 +6,23 @@ import path from 'path'
 import { assert } from 'console'
 
 const html = fs.readFileSync(path.resolve(__dirname, './student_schedule.html'), 'utf8');
-
 let dom
 let container 
+const start = "2022-01-01";
+const duration = 2;
+const end = "2022-01-15";
+const calculator = require('./HelperFunctions.js');
+describe('HelperFunctions.js', () => {
+
+    test('displays correct end date', ()=> {
+        expect(calculator.getEndDate(start, duration)).toBe(end);
+    })
+})
+
+describe('student_schedule.html', () => {
+    it('renders a button element', () =>{
+        dom = new JSDOM(html)
+        container = dom.window.document.body
+        expect(container.querySelector('button')).toBeInTheDocument()
+    })
+})
